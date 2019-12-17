1e3b5cdaffaec8e313d277a71c7d26ed5515d0eb
Jasper Van der Jeugt <jasper@fugue.co>
2019-12-17 16:43:40 +0100

    exit not necessary

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,13 @@ terraform show -json "$TERRAFORM_PLAN" >"$REGULA_INPUT"
 opa eval -i "$REGULA_INPUT" -d '/opt/regula' 'data.fugue.regula.report' \
     | tee "$REGULA_OUTPUT"
 
-FAILED=$(jq '.result[0].expressions[0].value.failed' "$REGULA_OUTPUT")
+FAILED=$(jq '.result[0].expressions[0].value.failed | @sh' "$REGULA_OUTPUT")
 NUM_PASSED=$(jq -r '.result[0].expressions[0].value.num_passed' "$REGULA_OUTPUT")
 NUM_FAILED=$(jq -r '.result[0].expressions[0].value.num_failed' "$REGULA_OUTPUT")
 VALID=$(jq -r '.result[0].expressions[0].value.valid' "$REGULA_OUTPUT")
+echo "$NUM_PASSED rules passed, $NUM_FAILED rules failed"
 echo "::set-output name=rules_passed::${NUM_PASSED}"
 echo "::set-output name=rules_failed::${NUM_FAILED}"
 if [[ "$VALID" != "true" ]]; then
     echo "::error ::Rules failed: ${FAILED}"
-    exit 1
 fi
